243f5a43e6e9b8e407db313b10d7109e90776e18
Boksha Dmitry <mapleleafs09@gmail.com>
2022-07-01 19:52:27 +0300

    related_name='measurements'

--- measurement/models.py
+++ measurement/models.py
@@ -7,7 +7,7 @@ class Sensor(models.Model):
     description = models.CharField(max_length=70)
 
 class Measurement(models.Model):
-    sensor_id = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    sensor_id = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
     temperature = models.CharField(max_length=10)
     datatime = models.DateTimeField(auto_now=True)
 
